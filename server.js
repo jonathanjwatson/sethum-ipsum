108eebb7edbe74b29508902448674170fb6482f1
jwats287 <jwats287@users.noreply.github.com>
2019-08-22 18:03:40 -0400

    pseudocoded and built out array of quotes

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+//need to store different quotes
+//array of strings
+let randomQuotes = [
+    "Instructions are not asynchronous",
+    "No Firewall = Butt Ass Open",
+    "There is no crying in coding",
+    "BLESS you",
+    "I am a Jelly Bean",
+    "There are no liferafts in bootcamp.",
+    "You seem a little lost",
+]
+
+//routes
+
+// /api/quotes
+// returns a collection of quotes
+// GET
+
+// /api/quotes/random
+// returns a single random quote
+// GET
+
+// /api/quotes
+// adds a new quote to the collection of quotes
+// POST
+// returns entire collection of quotes with new quote added
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log("Listening on PORT: " + PORT)
+});
