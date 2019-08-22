222c4581c2e96e1556d06efbb116e3063f66d80b
jwats287 <jwats287@users.noreply.github.com>
2019-08-22 18:27:33 -0400

    pseudocoded SQL

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //need to store different quotes
 //array of strings
@@ -37,6 +37,22 @@ app.get('/api/quotes', function(req, res) {
 // adds a new quote to the collection of quotes
 // POST
 // returns entire collection of quotes with new quote added
+
+app.post('/api/quotes', function(req, res) {
+    const newQuote = req.body.new;
+    // console.log(newQuote);
+    // randomQuotes.push(newQuote);
+    //OPTION 1 - fs.appendFile
+    //OPTION 2 - SQL
+    // require mysql
+    // setup the config for the instance
+    // write an insert query
+    // select * from query
+    // res.json the select query results
+
+
+    return res.json(randomQuotes);
+});
 
 
 
